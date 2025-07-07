ed9e45055d0f08a290f178cedfb20b47d29d86df
Luis Mattos <luis.mattos@rdstation.com>
2025-07-07 15:05:50 -0300

    feat: create models

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
+  
+  has_secure_password
+
+  has_many :posts
+  has_many :comments
+end
